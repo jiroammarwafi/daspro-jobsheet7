4f6d0b7c5752a554a726be2c89acb548d8ddc91c
jiroammarwafi <149659337+jiroammarwafi@users.noreply.github.com>
2024-10-11 10:48:00 +0700

    2nd Commit

--- SiakadFor14.java
+++ SiakadFor14.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class SiakadFor14{
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
-       double nilai, tertinggi = 0, terendah = 100;
+       double nilai, tertinggi = 0, terendah = 100, batasKelulusan = 60, lulus = 0, tidakLulus = 0;
 
-    for (int i = 1; i <= 10; i++) {
+    for (int i = 1; i <= 5; i++) {
         System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
         nilai = sc.nextDouble();
         if (nilai > tertinggi){
@@ -13,8 +13,15 @@ public class SiakadFor14{
         if (nilai < terendah){
             terendah = nilai;
         }
+        if (nilai > batasKelulusan){
+            nilai = lulus++ ;
+        } else {
+            nilai = tidakLulus++ ;
+        }
     }
     System.out.println("Nilai tertinggi: " + tertinggi);
     System.out.println("Nilai terendah: " + terendah);
+    System.out.println("Banyaknya lulus : " + (byte) lulus);
+    System.out.println("Banyaknya tidak lulus : " + (byte) tidakLulus);
     }
 }
